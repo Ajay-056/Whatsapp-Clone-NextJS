62e09649a2d84ba4290341f54a91722b99d2ee71
AK <balaajay19@gmail.com>
2021-07-13 10:28:44 +0530

    pages/Loading: Bug fix

--- pages/Loading.js
+++ pages/Loading.js
@@ -1,12 +1,13 @@
 // import Image from 'next/image';
 // import React from 'react';
+import Image from 'next/image';
 import { Circle } from 'better-react-spinkit';
 
 function Loading() {
   return (
     <center style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
       <div>
-        <img
+        <Image
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
           alt="whatsapp"
           style={{ marginBottom: 10 }}
